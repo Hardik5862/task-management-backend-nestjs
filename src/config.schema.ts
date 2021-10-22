c6141f384f65e8925a6104e8f58dc8df9ad62bda
Hardik5862 <67375963+Hardik5862@users.noreply.github.com>
2021-10-22 22:58:06 +0530

    deploying

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -1,6 +1,7 @@
 import * as Joi from 'joi';
 
 export const configValidationSchema = Joi.object({
+  PORT: Joi.number().default(3000),
   DB_HOST: Joi.string().required(),
   DB_PORT: Joi.number().default(5432).required(),
   DB_USERNAME: Joi.string().required(),
